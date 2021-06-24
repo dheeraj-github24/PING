bedbf496c4d2156585b42f807c4173449007cddf
Shreesh Chikkerur <shreeshchikkerur@gmail.com>
2021-06-24 16:16:03 +0530

    change user_item text color

--- app/src/main/java/com/example/ping02/searchforglobal.java
+++ app/src/main/java/com/example/ping02/searchforglobal.java
@@ -7,7 +7,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.SearchView;
 
 import com.example.ping02.Adapter.User_Adapter;
@@ -19,6 +22,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -27,7 +31,7 @@ import java.util.List;
 public class searchforglobal extends AppCompatActivity {
 
     private RecyclerView rv;
-    private SearchView sv;
+    private EditText sv;
     private User_Adapter user_adapter;
     private List<User> mUsers;
 
@@ -42,6 +46,53 @@ public class searchforglobal extends AppCompatActivity {
 
         mUsers=new ArrayList<>();
         readUsers();
+
+        sv=findViewById(R.id.searchView);
+        sv.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                searchUsers(s.toString());
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    private void searchUsers(String s) {
+        FirebaseUser firebaseUser=FirebaseAuth.getInstance().getCurrentUser();
+        Query query=FirebaseDatabase.getInstance().getReference("Users").orderByChild("Firstname")
+                .startAt(s.toUpperCase())
+                .endAt(s.toLowerCase()+'\uf8ff');
+
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                mUsers.clear();
+                for (DataSnapshot snapshot1:snapshot.getChildren()){
+                    User user=snapshot1.getValue(User.class);
+                    assert user != null;
+                    assert firebaseUser != null;
+                    if(!user.getid().equals(firebaseUser.getUid())){
+                        mUsers.add(user);
+                    }
+                }
+                user_adapter=new User_Adapter(getApplicationContext(),mUsers);
+                rv.setAdapter(user_adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     private void readUsers() {
@@ -51,17 +102,19 @@ public class searchforglobal extends AppCompatActivity {
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                mUsers.clear();
-                for(DataSnapshot snapshot1:snapshot.getChildren()){
-                    User user=snapshot1.getValue(User.class);
+                if (sv.getText().toString().equals("")) {
+                    mUsers.clear();
+                    for (DataSnapshot snapshot1 : snapshot.getChildren()) {
+                        User user = snapshot1.getValue(User.class);
 
-                    assert user != null;
-                    if(!user.getid().equals(firebaseUser.getUid())){
-                        mUsers.add(user);
+                        assert user != null;
+                        if (!user.getid().equals(firebaseUser.getUid())) {
+                            mUsers.add(user);
+                        }
                     }
+                    user_adapter = new User_Adapter(getApplicationContext(), mUsers);
+                    rv.setAdapter(user_adapter);
                 }
-                user_adapter=new User_Adapter(getApplicationContext(), mUsers);
-                rv.setAdapter(user_adapter);
             }
 
             @Override
