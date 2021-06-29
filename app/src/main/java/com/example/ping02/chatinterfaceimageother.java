45849850f24faf2a64a4ca5ec98c45ff651ba813
Ashish NK <ashish.nk.2524@gmail.com>
2021-06-29 23:53:20 +0530

    Images sending/receiving and typing... placed

---
+++ app/src/main/java/com/example/ping02/chatinterfaceimageother.java
@@ -0,0 +1,14 @@
+package com.example.ping02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class chatinterfaceimageother extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatinterfaceimageother);
+    }
+}
